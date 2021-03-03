78467d6d133cc204922c0e0a74a20ce0c4758300
lhb0927 <61721606+lhb0927@users.noreply.github.com>
2021-03-04 00:57:12 +0900

    init

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Input Numbers: "<<endl;
+    int x;
+    cin >>x;
+    cout << "Hello : "<<x<<endl;
+    return 0;
+}
